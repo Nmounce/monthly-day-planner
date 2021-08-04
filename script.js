8bab1e510e6aa7ea1494e7e50dd2709514d58585
NikkiMounce <nikkimounce@gmail.com>
2021-08-03 22:02:41 -0700

    and we are starting over

--- script.js
+++ script.js
@@ -1,92 +1,55 @@
-//WHEN I open the planner
-//THEN the current day is displayed at the top of the calendar
-const currentDate = moment().format('MM/DD/YYYY'); // get current date
-document.getElementById("currentDay").textContent = currentDate; // display inside the element
 
-jQuery(document).ready(function(){ 
-  iCal = new Web2Cal( "calendar",
-         { 
-              loadEvents: function(moment){ 
-                              /* Get events from any source. This can be a PHP/Java/.NET/Facebook or any other source. Once you have the data, invoke ical.render(data).*/
-                             ical.render( eventSource.getEvents() ); 
-                          } 
-             ,onNewEvent: function(event, groups, allDay){ } 
-             ,onUpdateEvent: function(event){ }
-         });
-         iCal.build(); 
-  }); 
+//set moment to a variable
+var now = moment();
+//create a variable for the current date
+var currentDate = now.format("MM DD YYYY");
 
+//create calendar grid
+const container = $(".container");
+const times = ["6 AM", "7 AM", "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM", "6 PM", "7 PM", "8 PM"];
 
- //datepicker module 
-  $( function() {
-    $( "#datePicker" ).datepicker();
-  } );
+function timeBlocks() {
+    for (let i = 0; i < times.length; i++) {
+        const row = $("<div>");
+        const time = $("<div>");
+        const textarea = $("<textarea>");
+        const save = $("<button>");
+        row.addClass("row");
+        time.text(times[i]).addClass("time-block", "col-1");
+        textarea.addClass("col-10");
+        save.text("SAVE").addClass("saveBtn", "col-1").attr("id", times[i]);
+        container.append(row);
+        row.append(time, textarea, save);
 
-  $( function() {
-    $( "#calendar" ).selectable();
-  } );
-
-$( "#calendar" ).click(function(e) {
-
-    let row = 0, length = 1, column = 0
-    let timeObj = {
-        "5 AM" : 1,
-        "6 AM" : 2,
-        "7 AM" : 3,
-        "8 AM" : 4,
-        "9 AM" : 5,
-        "10 AM" : 6,
-        "11 AM" : 7,
-        "12 PM" : 8,
-        "1 PM" : 9,
-        "2 PM" : 10,
-        "3 PM" : 11,
-        "4 PM" : 12,
-        "5 PM" : 13,
-        "6 PM" : 14,
-        "7 PM" : 15,
-        "8 PM" : 16,
-        "9 PM" : 17,
-        "10 PM" : 18,
-        "11 PM" : 19,
-        "12 AM" : 20,
-        "1 AM" : 21,
-        "2 AM" : 22,
-        "3 AM" : 23,
-        "4 AM" : 24,
+        save.on("click", function(event) {
+            console.log($(this).attr("id"));
+            console.log($(this).prev().val());
+        })
     }
+}
+timeBlocks();
 
-    $('#modal').toggle();
+//--onclick event to save user input to local storage---//
+$(".saveBtn").on("click", function() {
+    var hour = $(this).attr("id");
+    var textInput = $(this).prev().val();
 
-    $('#modal').click(function(){
-        $('#modal').css('display', 'none')
-    });
-    $('#modal-container-form').submit(function(e){
-        e.preventDefault()
-        row = timeObj[$( "#start-select" ).val()]
-        length = (timeObj[$( "#end-select" ).val()]) - (timeObj[$( "#start-select" ).val()])
-        $('#calendar').append(`<div class='event' style='grid-row:${row}/span ${length};grid-column:${column};background-color: yellow;'>Details</div>`)
-        $('#modal-container-form')[0].reset()
-        $('#modal-container-form').off()
-        $('#modal').toggle()       })
-  });
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    localStorage.setItem(hour, textInput);
+    console.log(hour, textInput);
+});
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
+//color change function
 
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-// ```
-
+console.log(
+now.hour())
+for (let i = 0; i < times.length; i++) {
+    var timeSlot = times[i].split(" ")[0]
+    console.log(timeSlot)
+    if (timeSlot < now.hour()) {
+        $(`#${times[i]}`).prev().addClass("past")
+    } else if (timeSlot > now.hour()) {
+        $(`#${times[i]}`).prev().addClass("future")
+    }else {
+        $(`#${times[i]}`).prev().addClass("present")
+    }
+};
